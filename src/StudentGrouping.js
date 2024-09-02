de80ca0f6150f9833dc6c5f47e833185b10020c1
isaac john <isaacjohn2028@gmail.com>
2024-09-03 00:38:25 +0100

    first commit

---
+++ src/StudentGrouping.js
@@ -0,0 +1,75 @@
+import React, { useState,  } from 'react';
+import { studentsData } from './data';
+
+
+const StudentGrouping = () => {
+  const [groupSize, setGroupSize] = useState();
+  const [groups, setGroups] = useState([]);
+
+  const shuffleArray = (array) => {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  };
+
+  const generateGroups = (students, size, random = false) => {
+    const sortedStudents = random
+      ? shuffleArray(students)
+      : [...students].sort((a, b) => a.id - b.id);
+
+    const newGroups = [];
+    for (let i = 0; i < sortedStudents.length; i += size) {
+      newGroups.push(sortedStudents.slice(i, i + size));
+    }
+    setGroups(newGroups);
+  };
+
+  const handleRandomGroups = () => {
+    generateGroups(studentsData, groupSize, true);
+  };
+
+  const handleIdGroups = () => {
+    generateGroups(studentsData, groupSize, false);
+  };
+
+  return (
+    <div className="p-4">
+      <div className="mb-4">
+        <input
+          type="number"
+          value={groupSize}
+          onChange={(e) => setGroupSize(parseInt(e.target.value))}
+          placeholder="Enter group size"
+          className="mr-2"
+        />
+        <div>
+            <button onClick={handleRandomGroups} className="mr-2">Generate Random Groups</button>
+            <button onClick={handleIdGroups}>Generate Groups by ID</button>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {groups.map((group, index) => (
+          <div key={index}>
+            <div>
+              <div style={{fontWeight: 700, fontSize: '1rm'}}>Group {index + 1}</div>
+            </div>
+            <div>
+              <ul>
+                {group.map((student) => (
+                  <li key={student.id} >
+                    {student.name} ----- 23/{student.Rgn}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default StudentGrouping;
---
+++ src/data.js
@@ -0,0 +1,650 @@
+export const studentsData = [
+    { 
+        id: 1, 
+        name: "Enoch Bulus Ibrahim", 
+        Rgn: "0061" 
+    },
+    { 
+        id: 2, 
+        name: "Yahaya Muazu Zakarya", 
+        Rgn: "0085" 
+    },
+    { 
+        id: 3, 
+        name: "Hanifah Mikail", 
+        Rgn: "0538" 
+    },
+    { 
+        id: 4, 
+        name: "Jamiu Mikail", 
+        Rgn: "0578" 
+    },
+    { 
+        id: 5, 
+        name: "Mubarak Sunusi", 
+        Rgn: "0002" 
+    },
+    { 
+        id: 6, 
+        name: "Saadatu Idris Muhammad", 
+        Rgn: "0494" 
+    },
+    { 
+        id: 7, 
+        name: "Ukasha Abdulkadir", 
+        Rgn: "0205" 
+    },
+    { 
+        id: 8, 
+        name: "Abulfatahi Almustapha Makarant", 
+        Rgn: "0605" 
+    },
+    { 
+        id: 9, 
+        name: "Watsav Iyua Torkuma", 
+        Rgn: "0503" 
+    },
+    { 
+        id: 10, 
+        name: "Funsho Simon Ipinmoroti", 
+        Rgn: "0417" 
+    },
+    { 
+        id: 12, 
+        name: "Auwal Galadima Isma", 
+        Rgn: "1049" 
+    },
+    { 
+        id: 13, 
+        name: "Bright Gweinmen Gandu", 
+        Rgn: "0750" 
+    },
+    { 
+        id: 14, 
+        name: "Yahaya Suleiman", 
+        Rgn: "0668" 
+    },
+
+    { 
+        id: 15, 
+        name: "Mubarak Bala", 
+        Rgn: "0087" 
+    },
+    { 
+        id: 16, 
+        name: "Aminu Zakari Yau", 
+        Rgn: "0293" 
+    },
+    { 
+        id: 17, 
+        name: "Nenman Samuel", 
+        Rgn: "0546" 
+    },
+    { 
+        id: 18, 
+        name: "Isaac John Manga", 
+        Rgn: "0501" 
+    },
+    { 
+        id: 19, 
+        name: "Umar Sa'adatu", 
+        Rgn: "0502" 
+    },
+    { 
+        id: 20, 
+        name: "Sani Idris", 
+        Rgn: "0228" 
+    },
+    { 
+        id: 21, 
+        name: "Ephraim Francis Edem", 
+        Rgn: "0001" 
+    },
+    { 
+        id: 22, 
+        name: "Vershima Matthew Yin", 
+        Rgn: "0003" 
+    },
+    { 
+        id: 23, 
+        name: "Rashida Umar", 
+        Rgn: "0423" 
+    },
+    { 
+        id: 24, 
+        name: "Aliyu Zakar", 
+        Rgn: "0099" 
+    },
+    { 
+        id: 25, 
+        name: "Abdulaziz Ahmed", 
+        Rgn: "0536" 
+    },
+
+    { 
+        id: 26, 
+        name: "Magdaline Patience Dauda", 
+        Rgn: "0145" 
+    },
+    { 
+        id: 27, 
+        name: "Bilkisu Lukman Yusu", 
+        Rgn: "0550" 
+    },
+    { 
+        id: 28, 
+        name: "Umar Abdulmalik", 
+        Rgn: "0460" 
+    },
+    { 
+        id: 29, 
+        name: "Muhammad Abdullahi", 
+        Rgn: "0354" 
+    },
+    { 
+        id: 30, 
+        name: "Oluwatobi James Adekunl", 
+        Rgn: "1069" 
+    },
+    { 
+        id: 31, 
+        name: "Isah Muhammad", 
+        Rgn: "0057" 
+    },
+    { 
+        id: 32, 
+        name: "Abdulrazaktajudeen", 
+        Rgn: "0595" 
+    },
+    { 
+        id: 33, 
+        name: "Joy Nnenna Opara", 
+        Rgn: "0251" 
+    },
+    { 
+        id: 34, 
+        name: "Mustapha Jibril", 
+        Rgn: "0289" 
+    },
+    { 
+        id: 35, 
+        name: "Yusuf Lawal Samanja", 
+        Rgn: "0601" 
+    },
+    { 
+        id: 36, 
+        name: "Onamza Inus", 
+        Rgn: "0018" 
+    },
+    { 
+        id: 37, 
+        name: "Adebowale Adewale Isaac", 
+        Rgn: "0124" 
+    },
+    { 
+        id: 38, 
+        name: "Zainab Taiwo", 
+        Rgn: "0421" 
+    },
+    { 
+        id: 39, 
+        name: "Abdulhadi Musa", 
+        Rgn: "0777" 
+    },
+    { 
+        id: 40, 
+        name: "David Isiaka", 
+        Rgn: "0386" 
+    },
+    { 
+        id: 41, 
+        name: "Nabilah Shuaib", 
+        Rgn: "0447" 
+    },
+    { 
+        id: 42, 
+        name: "Yahaya Rilwa", 
+        Rgn: "0692" 
+    },
+    { 
+        id: 43, 
+        name: "Jamilu Muhammad", 
+        Rgn: "0101" 
+    },
+    { 
+        id: 44, 
+        name: "Ibrahim Khalil", 
+        Rgn: "0473" 
+    },
+    { 
+        id: 45, 
+        name: "Rashidat Omolabake Isiak", 
+        Rgn: "0424" 
+    },
+    { 
+        id: 46, 
+        name: "Victor Kerkebe Sila", 
+        Rgn: "0410" 
+    },
+    { 
+        id: 47, 
+        name: "Caleb Alejeudo", 
+        Rgn: "0565" 
+    },
+    { 
+        id: 48, 
+        name: "Isah Rukaya Oniz", 
+        Rgn: "0422" 
+    },
+    { 
+        id: 49, 
+        name: "Samuel Orolan", 
+        Rgn: "0331" 
+    },
+    { 
+        id: 50, 
+        name: "Ridwan Muhamma", 
+        Rgn: "0555" 
+    },
+    { 
+        id: 51, 
+        name: "Umar Nuhu", 
+        Rgn: "0080" 
+    },
+    { 
+        id: 52, 
+        name: "Jibril Hami", 
+        Rgn: "0663" 
+    },
+    { 
+        id: 53, 
+        name: "Jesse Charle", 
+        Rgn: "0270" 
+    },
+    { 
+        id: 54, 
+        name: "Amiru Muhammed Yakub", 
+        Rgn: "0650" 
+    },
+    { 
+        id: 55, 
+        name: "Kauthar Ishaq", 
+        Rgn: "0084" 
+    },
+    { 
+        id: 56, 
+        name: "Farida Bell", 
+        Rgn: "0493" 
+    },
+    { 
+        id: 57, 
+        name: "Adamu Tahiru", 
+        Rgn: "1055" 
+    },
+    { 
+        id: 58, 
+        name: "Musa Auwal", 
+        Rgn: "1054" 
+    },
+    { 
+        id: 59, 
+        name: "Rukayya Mustapha", 
+        Rgn: "0076" 
+    },
+    { 
+        id: 60, 
+        name: "Moses Chukwuebuka Odoh", 
+        Rgn: "0407" 
+    },
+    { 
+        id: 61, 
+        name: "Abdulaziz Umar ", 
+        Rgn: "0685" 
+    },
+    { 
+        id: 62, 
+        name: "Ahmed Ifedolapo Adeniy", 
+        Rgn: "0335" 
+    },
+    { 
+        id: 63, 
+        name: "Temidayo David Oludar", 
+        Rgn: "0069" 
+    },
+    { 
+        id: 64, 
+        name: "Ikyavtese Didimus Aligba", 
+        Rgn: "0072" 
+    },
+    { 
+        id: 65, 
+        name: "Rilwanulah Olaitan Zakariya", 
+        Rgn: "0164" 
+    },
+    { 
+        id: 66, 
+        name: "Abdulmalik Abdulyakee", 
+        Rgn: "0716" 
+    },
+    { 
+        id: 67, 
+        name: "Emmanuel Ordue Lumung", 
+        Rgn: "0775" 
+    },
+    { 
+        id: 68, 
+        name: "Justice Ma", 
+        Rgn: "0059" 
+    },
+    { 
+        id: 66, 
+        name: "Yusuf Lawal Abdulsalam", 
+        Rgn: "1060" 
+    },
+    { 
+        id: 70, 
+        name: "Lawal Sherifdeen Babatunde", 
+        Rgn: "0298" 
+    },
+    { 
+        id: 71, 
+        name: "Ayuba Onimisi Raji", 
+        Rgn: "0150" 
+    },
+    { 
+        id: 72, 
+        name: "Samuel Agbo", 
+        Rgn: "0382" 
+    },
+    { 
+        id: 73, 
+        name: "Abdullahi Kabi", 
+        Rgn: "0139" 
+    },
+    { 
+        id: 74, 
+        name: "Murtala Barau", 
+        Rgn: "0525" 
+    },
+    { 
+        id: 75, 
+        name: "Samuel Romanus", 
+        Rgn: "0161" 
+    },
+    { 
+        id: 76, 
+        name: "Isaac Onosied", 
+        Rgn: "0665" 
+    },
+    { 
+        id: 77, 
+        name: "Sumayya Isyaku Yunus", 
+        Rgn: "0098" 
+    },
+    { 
+        id: 78, 
+        name: "Peter ", 
+        Rgn: "0570" 
+    },
+    { 
+        id: 79, 
+        name: "Abdulsalam Zikrullah", 
+        Rgn: "0201" 
+    },
+    { 
+        id: 80, 
+        name: "Najiyah Ohunene Idri", 
+        Rgn: "0738" 
+    },
+    { 
+        id: 81, 
+        name: "Rofiat Ayoni Egbeyemi", 
+        Rgn: "0484" 
+    },
+    { 
+        id: 82, 
+        name: "Rachael Matthew ", 
+        Rgn: "0664" 
+    },
+    { 
+        id: 83, 
+        name: "Rahilla Mshelia Elifas", 
+        Rgn: "0051" 
+    },
+    { 
+        id: 84, 
+        name: "Ibrahim Abubakar", 
+        Rgn: "0543" 
+    },
+    { 
+        id: 85, 
+        name: "Azeezat Usman", 
+        Rgn: "0576" 
+    },
+    { 
+        id: 86, 
+        name: "Samira Mustapha", 
+        Rgn: "0338" 
+    },
+    { 
+        id: 87, 
+        name: "Richard Musa Andrew", 
+        Rgn: "0261" 
+    },
+    { 
+        id: 88, 
+        name: "Onyinyechi Esther Stanle", 
+        Rgn: "0706" 
+    },
+    { 
+        id: 89, 
+        name: "Usman Ngulde Yunusa", 
+        Rgn: "0292" 
+    },
+    { 
+        id: 90, 
+        name: "Faridat Jibril Abdullah", 
+        Rgn: "0662" 
+    },
+    { 
+        id: 91, 
+        name: "Adenike Abdulsala", 
+        Rgn: "0474" 
+    },
+    { 
+        id: 92, 
+        name: "Abubakar Moshood", 
+        Rgn: "0325" 
+    },
+    { 
+        id: 93, 
+        name: "Mustapha Hamisu", 
+        Rgn: "1050" 
+    },
+    { 
+        id: 94, 
+        name: "Aliyu Magaji Isa", 
+        Rgn: "0599" 
+    },
+    { 
+        id: 95, 
+        name: "Mubarak Moshood", 
+        Rgn: "0671" 
+    },
+    { 
+        id: 96, 
+        name: "Mustapha Abdullahi", 
+        Rgn: "0572" 
+    },
+    { 
+        id: 97, 
+        name: "Zainab Abdulwa", 
+        Rgn: "0589" 
+    },
+    { 
+        id: 98, 
+        name: "Hassan Jibril", 
+        Rgn: "0564" 
+    },
+    { 
+        id: 99, 
+        name: "Abubakar Shafiu", 
+        Rgn: "0159" 
+    },
+    { 
+        id: 100, 
+        name: "Sarah Omolara Aye", 
+        Rgn: "0234" 
+    },
+    { 
+        id: 101, 
+        name: "Muhammad Sani Yunus", 
+        Rgn: "0520" 
+    },
+    { 
+        id: 102, 
+        name: "Asher Dype Reuben", 
+        Rgn: "0388" 
+    },
+    { 
+        id: 103, 
+        name: "Al-Amin Adam Muhamma", 
+        Rgn: "0440" 
+    },
+    { 
+        id: 104, 
+        name: "Muhammad Salim Muhammad", 
+        Rgn: "0151" 
+    },
+    { 
+        id: 105, 
+        name: "Kelechukwu Ezenwamm", 
+        Rgn: "0362" 
+    },
+    { 
+        id: 106, 
+        name: "Abdulrashid Misbaudeen", 
+        Rgn: "0749" 
+    },
+    { 
+        id: 107, 
+        name: "Dawud Yahay", 
+        Rgn: "0158" 
+    },
+    { 
+        id: 108, 
+        name: "Bilyamin Hudu", 
+        Rgn: "0005" 
+    },
+    { 
+        id: 109, 
+        name: "Zainab Oyiza Yusuf", 
+        Rgn: "0527" 
+    },
+    { 
+        id: 110, 
+        name: "Hashim Magaga Abdulsalam", 
+        Rgn: "0619" 
+    },
+    { 
+        id: 111, 
+        name: "Tola Innocent Edwar", 
+        Rgn: "0115" 
+    },
+    { 
+        id: 112, 
+        name: "Precious Chiamaka Edward", 
+        Rgn: "0195" 
+    },
+    { 
+        id: 113, 
+        name: "Muhammad Kabir Halir", 
+        Rgn: "0552" 
+    },
+    { 
+        id: 114, 
+        name: "Muhammad Buhari Kachalla", 
+        Rgn: "0759" 
+    },
+    { 
+        id: 115, 
+        name: "Nasiru Aminu", 
+        Rgn: "0445" 
+    },
+    { 
+        id: 116, 
+        name: "Usman Suleima", 
+        Rgn: "0743" 
+    },
+    { 
+        id: 117, 
+        name: "Lukman Asipita Nasir", 
+        Rgn: "0095" 
+    },
+    { 
+        id: 118, 
+        name: "Fatima Lawa", 
+        Rgn: "0066" 
+    },
+    { 
+        id: 119, 
+        name: "Fatimah Oluwabukola Alimi", 
+        Rgn: "0497" 
+    },
+    { 
+        id: 120, 
+        name: "Umar Faruk Abdullah", 
+        Rgn: "0496" 
+    },
+    { 
+        id: 121, 
+        name: "Timothy Orowole", 
+        Rgn: "0626" 
+    },
+    { 
+        id: 122, 
+        name: "Bilkisu Murtala23/010", 
+        Rgn: "1070" 
+    },
+    { 
+        id: 123, 
+        name: "Abdulsalam Kolawol", 
+        Rgn: "0621" 
+    },
+    { 
+        id: 124, 
+        name: "Usama Sheh", 
+        Rgn: "0612" 
+    },
+    { 
+        id: 125, 
+        name: "Kinsman Adavize Omanan", 
+        Rgn: "0120" 
+    },
+    { 
+        id: 126, 
+        name: "Hauwau Nuhu Muhamma", 
+        Rgn: "0521" 
+    },
+    { 
+        id: 127, 
+        name: "Safunat Oyiza Muhamme", 
+        Rgn: "0053" 
+    },
+    { 
+        id: 128, 
+        name: "Joseph Danlad", 
+        Rgn: "0239" 
+    },
+    { 
+        id: 129, 
+        name: "Chisom Amed", 
+        Rgn: "0703" 
+    },
+    { 
+        id: 131, 
+        name: "Yusuf Hamza", 
+        Rgn: "0680" 
+    },
+    
+]
